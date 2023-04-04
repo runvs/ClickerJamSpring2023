0f2a5087af024999e23d6c3453eabc17ec01d1b7
Thunraz <thunraz@thunraz.com>
2023-04-04 17:48:05 +0200

    Add and use savegame JavaScript file

---
+++ assets_src/savegame.js
@@ -0,0 +1,8 @@
+function save(data) {
+    console.log('saving data: ', data);
+}
+
+function load() {
+    console.log('loading (static) data');
+    return "{\"bank\":{\"money\":{\"value\":[117,2]}},\"buttons\":{\"Blaster\":{\"value\":[16,39]},\"Driller\":{\"value\":[232,3]},\"Geologist\":{\"value\":[100]},\"Miner\":{\"value\":[16]}},\"purchased\":{\"Blaster\":0,\"Driller\":0,\"Geologist\":0,\"Miner\":3}}\n";
+}
